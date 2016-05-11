11a4b119504b53489febafcfd2886300cf60bbe0
dsuare1 <suarez.derrick@gmail.com>
2016-05-10 20:09:32 -0500

    almost final updates

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -4,6 +4,7 @@ window.onload = function() {
     var wins = 0;
     var remGuesses = 9;
     var lettersGuessed = [];
+    var correctLettersGuessed = [];
 
     // creates an array of characters from which to choose for current character
     var chars = ['frodo', 'sam', 'pippin', 'merry', 'aragorn', 'boromir', 'legolas', 'gimli', 'gandalf', 'gollum', 'galadriel', 'arwen', 'elrond', 'celeborn', 'theoden', 'eomer', 'faramir', 'treebeard', 'isildur', 'denethor', 'sauron', 'saruman', 'grima', 'shelob', 'nazgul'];
@@ -11,6 +12,14 @@ window.onload = function() {
     // selects random character from chars array
     var curWord = chars[Math.floor(Math.random() * chars.length)].toLowerCase();;
     console.log(curWord);
+
+    // variable to hold char img source
+    var imgSrc = "src='assets/images/" + curWord + ".jpg' alt='Character Image'";
+    console.log(imgSrc);
+
+    // pick image relating to character
+    document.querySelector("#left").innerHTML = "<img id='img-hint'" + imgSrc + ">";
+
 
     // creates the active word array so it can be displayed as underscores
     var activeWord = [];
@@ -24,9 +33,9 @@ window.onload = function() {
     var remainingLetters = curWord.length;
     console.log(remainingLetters);
 
-    
 
-// Display HTML & variables
+
+    // Display HTML & variables
     // sets wins to display as 0
     document.querySelector('#wins').innerHTML = "Wins:<br>" + wins;
 
@@ -37,14 +46,24 @@ window.onload = function() {
     document.querySelector('#current-word').innerHTML = "Current character:<br>" + activeWord.join(" ");
 
 
-// reset game function
+    // reset game function
     function resetGame() {
+
+        console.log("reset was called");
 
         wins = 0;
         remGuesses = 9;
         lettersGuessed = [];
+        correctLettersGuessed = [];
         curWord = chars[Math.floor(Math.random() * chars.length)].toLowerCase();;
         console.log(curWord);
+
+        // variable to hold char img source
+        imgSrc = "src='assets/images/" + curWord + ".jpg' alt='Character Image'";
+        console.log(imgSrc);
+
+        // pick image relating to character
+        document.querySelector("#left").innerHTML = "<img id='img-hint'" + imgSrc + ">";
 
         // creates the active word array so it can be displayed as underscores
         activeWord = [];
@@ -59,6 +78,8 @@ window.onload = function() {
         remainingLetters = curWord.length;
         console.log(remainingLetters);
 
+        document.querySelector('h3').innerHTML = "Press any key to begin your quest!";
+
         // sets wins to display as 0
         document.querySelector('#wins').innerHTML = "Wins:<br>" + wins;
 
@@ -68,7 +89,9 @@ window.onload = function() {
         // displays the curWord as dashes
         document.querySelector('#current-word').innerHTML = "Current character:<br>" + activeWord.join(" ");
 
-        document.querySelector('#current-word').innerHTML = "Current character:<br>" + activeWord.join(" ");
+        document.querySelector('#letters-guessed').innerHTML = "Letters already guessed:<br>";
+
+        document.querySelector('#column-wrapper').style.display = "block";
     }
 
     // initializes gameplay w/ any keystroke
@@ -78,6 +101,11 @@ window.onload = function() {
         var userGuess = String.fromCharCode(event.keyCode).toLowerCase();
         console.log(userGuess);
 
+        // check activeWord array to see if correctly guessed letter is already present
+        if ((activeWord.indexOf(userGuess) !== -1)) {
+            return;
+        }
+
         // check to see if userGuess is in the word; if so, update the 'activeWord' array with the userGuess
         for (var j = 0; j < curWord.length; j++) {
             if (curWord.charAt(j) === userGuess) {
@@ -85,23 +113,20 @@ window.onload = function() {
                 remainingLetters--;
                 console.log(remainingLetters);
                 document.querySelector('#current-word').innerHTML = "Current character:<br>" + activeWord.join(" ");
+                console.log(activeWord);
             }
         }
 
         // // checks to see if userGuess is NOT in the word AND that letter is already in the "letters already guessed" array to not duplicate
         // // that letter in the array   --> DOESN'T WORK
-
-        if (!curWord.includes(userGuess) && lettersGuessed.includes(userGuess)) {
+        if ((curWord.indexOf(userGuess) === -1) && (lettersGuessed.indexOf(userGuess) !== -1)) {
             document.querySelector('#letters-guessed').innerHTML = "Letters already guessed:<br>" + lettersGuessed.join(", ");
+            return;
         }
 
         // check to see if userGuess is NOT in the word; if it is not, push userGuess to the "Letters already guessed" array
         for (var k = 0; k < curWord.length; k++) {
-            // if ((!curWord.includes(userGuess)) && (lettersGuessed.includes(userGuess))) {
-            //     lettersGuessed.pop(userGuess);
-            //     break;
-            // } else 
-            if (!curWord.includes(userGuess)) {
+            if (curWord.indexOf(userGuess) === -1) {
                 lettersGuessed.push(userGuess)
                 remGuesses--;
                 break;
@@ -113,12 +138,14 @@ window.onload = function() {
         // }
 
         if (remainingLetters === 0) {
-            alert("Nice job!  Get ready for the next character!");
-            // document.querySelector("h3").innerHTML = "Nice job!  Get ready for the next character!";
-            setTimeout(resetGame(), 3000);
+            // alert("Nice job!  Get ready for the next character!");
+            document.querySelector("h3").innerHTML = "Nice job!  Get ready for the next character!";
+            document.querySelector("#column-wrapper").style.display = "none";
+            setTimeout(resetGame, 3000);
         } else if (remGuesses === 0) {
-            alert("You ran out of guesses!  Get ready for the next word...");
-            setTimeout(resetGame(), 3000);
+            document.querySelector("h3").innerHTML = "You ran out of guesses!  Get ready for the next word...";
+            document.querySelector("#column-wrapper").style.display = "none";
+            setTimeout(resetGame, 3000);
         }
 
         document.querySelector('#remaining-guesses').innerHTML = "Remaining guesses:<br>" + remGuesses;
